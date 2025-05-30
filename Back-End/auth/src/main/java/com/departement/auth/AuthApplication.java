05c70456fb2a038081d7ef83c39cf4feb0684a92
zakaria oumghar <zakariaoumghar1@gmail.com>
2025-05-30 16:30:07 +0100

    now its working I just need to remove the parent pom

--- Back-End/auth/src/main/java/com/departement/auth/AuthApplication.java
+++ Back-End/auth/src/main/java/com/departement/auth/AuthApplication.java
@@ -4,18 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @RequestMapping()
 public class AuthApplication {
 	@GetMapping("/auth")
 	public String auth() {
 		return "Hello Auth";
-	}
-
-	@GetMapping("/")
-	public String home() {
-		return "Common Models Service is running";
 	}
 
 	public static void main(String[] args) {
--- Back-End/chef-dep/src/main/java/com/departement/chefdep/ChefDepApplication.java
+++ Back-End/chef-dep/src/main/java/com/departement/chefdep/ChefDepApplication.java
@@ -4,18 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @RequestMapping
 public class ChefDepApplication {
     @GetMapping("/chefdep")
     public String chefdep() {
         return "Hello ChefDep";
-    }
-
-    @GetMapping("/")
-    public String home() {
-        return "Common Models Service is running";
     }
 
     public static void main(String[] args) {
