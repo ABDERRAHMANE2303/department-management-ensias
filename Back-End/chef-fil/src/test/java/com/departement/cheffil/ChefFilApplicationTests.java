1c2ad5abc27fc187ca9f288ca4d20d69fd8dc87f
zakaria oumghar <zakariaoumghar1@gmail.com>
2025-06-02 18:02:11 +0100

    adding chef fil

---
+++ Back-End/chef-fil/src/test/java/com/departement/cheffil/ChefFilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.departement.cheffil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefFilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
